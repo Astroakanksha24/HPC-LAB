4e9e5b5239fba511095c7a436d29fd6e516be8df
Astroakanksha <62433778+Astroakanksha24@users.noreply.github.com>
2022-09-26 11:50:02 +0530

    Create vec-vec-Par.c

---
+++ Assignment2/vec-vec-Par.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<omp.h>
+#include <pthread.h>
+ 
+void main()
+{
+    int N=100000;
+    int A[100000];
+ 
+    for(int i=1;i<=N;i++) A[i] = i+1;
+ 
+    int B[100000];
+ 
+    for(int i=1;i<=N;i++) B[i] = i;
+ 
+    int C[100000]={0};
+ 
+    double itime,ftime,exe_time;
+ 
+    itime=omp_get_wtime();
+ 
+    #pragma omp parallel for reduction(+ : C)
+    for(int i=1;i<=N;i++)
+    {
+        C[i]=A[i]+B[i];
+        printf("Thread Number %d",omp_get_thread_num());
+        printf("%d\n ",C[i]);
+    }    
+ 
+    ftime=omp_get_wtime();
+    exe_time=ftime-itime;
+    printf("\n Time taken is %d ",exe_time);
+ 
+}
